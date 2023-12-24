99aa2b3ffff041e4ef81936f3bdeece78912ac6d
eslemozluk <ozlukeslem@gmail.com>
2023-12-24 17:50:39 +0300

    123

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -13,20 +13,102 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Homepage",
-            style:
-                TextStyle(color: textDarkColor, fontWeight: FontWeight.w300)),
-        backgroundColor: primaryLightColor,
-        centerTitle: true,
-      ),
-      drawer: CompDrawer(),
-      body: Center(
-        child: Text(
-          "Anasayfa",
-          style: TextStyle(color: backgroundDarkColor),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              top: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  "assets/image/tobeto-logo.png",
+                  width: 200,
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: backgroundLightColor,
+          centerTitle: true,
         ),
-      ),
-    );
+        endDrawer: CompDrawer(),
+        body: ListView(
+          children: [
+            Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Color.fromARGB(31, 238, 238, 238),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "TOBETO",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(
+                                    color: primaryLightColor,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 40,
+                                  ),
+                            ),
+                            TextSpan(
+                              text: " 'ya ",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 30,
+                                  ),
+                            ),
+                            TextSpan(
+                              text: "Hoşgeldiniz",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 30,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text("suphiye eslem",
+                          style:
+                              Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 30,
+                                  )),
+                      //birinci kısım
+                      const SizedBox(height: 50),
+
+                      Text(
+                        "Yeni nesil öğrenme deneyimi ile Tobeto kariyer yolculuğunda senin yanında!",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        ));
   }
 }
